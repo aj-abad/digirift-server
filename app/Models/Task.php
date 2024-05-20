c11ea1be47d2e08dde659fa03efff4931017dd27
AJ <dev@ajabad.com>
2024-05-20 13:41:45 +0800

    Update Tasks model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,16 +2,10 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-class Task extends Authenticatable
+class Task extends Model
 {
-    use HasFactory, Notifiable, HasApiTokens;
-
     /**
      * The attributes that are mass assignable.
      *
