0615bb22b8c33a8460b282a953fa430397a9eb90
AJ <dev@ajabad.com>
2024-05-20 13:41:34 +0800

    Seeder changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Task;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
@@ -17,14 +18,32 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
+            'id' => 1,
             'name' => 'Tim Henson',
             'email' => 'tim@example.com',
             'password' => Hash::make('tim123'),
         ]);
         User::factory()->create([
+            'id' => 2,
             'name' => 'Scott LePage',
             'email' => 'scott@example.com',
             'password' => Hash::make('scott123'),
         ]);
+
+        foreach ([
+            [
+                'name' => 'Task 1',
+                'user_id' => 1,
+                'status' => 'todo'
+            ],
+            [
+                'name' => 'Task 2',
+                'user_id' => 2,
+                'status' => 'todo'
+            ],
+        ] as $task) {
+            $taskModel = new Task($task);
+            $taskModel->save();
+        }
     }
 }
